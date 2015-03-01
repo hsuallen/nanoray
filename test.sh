b166a293256e162ad4a8a80e3246d1a93e2bcd30
Allen Hsu <hsu.y.allen@gmail.com>
2015-03-01 17:10:09 -0500

    Modified test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 clang -Weverything -pedantic -std=c11 -Os test.c \
-	intersect.c vector.c \
-	intersect_test.c vector_test.c \
+	intersect.c scene.c vector.c \
+	intersect_test.c scene_test.c vector_test.c \
 	-o test-nanoray && ./test-nanoray
